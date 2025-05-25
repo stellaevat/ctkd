5f2fbd2874ff27337498de33dc6173e535319acd
Stella Eva Tsiapali <2526934t@student.gla.ac.uk>
2025-05-25 15:18:05 +0100

    Implemented DSKD distillation module

---
+++ src/dskd_distiller.py
@@ -0,0 +1,110 @@
+import os
+import json
+import torch
+import torch.nn as nn
+from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM
+from transformers.modeling_outputs import CausalLMOutput
+
+from dskd_loss import dskd_loss_fn
+
+
+class KD(nn.Module):
+    def __init__(self, args, device):
+        super(KD, self).__init__()
+        self.args = args
+        self.device = device
+
+        self.s_model, self.s_tokenizer, self.s_hidden_size = self._load_pretrained(args.s_path, args.s_type, args.s_dtype)
+        self.t_model, self.t_tokenizer, self.t_hidden_size = self._load_pretrained(args.t_path, args.t_type, args.t_dtype)
+        self.projectors = self._load_projectors(args.proj_path)
+
+        self.kd_loss_fn = dskd_loss_fn
+        self.ce_loss_fn = nn.CrossEntropyLoss()
+        self.kl_div_fn = nn.KLDivLoss(reduction='none')
+        self.weighted_loss = lambda ce_loss, kd_loss: (1 - args.kd_weight) * ce_loss + args.kd_weight * kd_loss
+        
+
+    def _load_pretrained(self, model_path, model_type, model_dtype):
+        config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
+        hidden_size = config.n_embed if hasattr(config, "n_embed") else config.hidden_size
+        
+        model = AutoModelForCausalLM.from_pretrained(model_path, config=config, torch_dtype=model_dtype, trust_remote_code=True)
+        model.gradient_checkpointing_enable()
+
+        tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        tokenizer.eos_token_id = 151643 if model_type == "qwen" else tokenizer.eos_token_id
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+        return model, tokenizer, hidden_size
+    
+
+    def _load_projectors(self, proj_path):
+        projectors = nn.ModuleDict()
+        projectors["s2t"] = nn.Linear(self.s_hidden_size, self.t_hidden_size)
+        projectors["t2s"] = nn.Linear(self.t_hidden_size, self.s_hidden_size)
+        projectors["query"] = nn.Linear(2 * self.s_hidden_size, 2 * self.t_hidden_size)
+        
+        if os.path.exists(proj_path):
+            params = torch.load(proj_path, map_location=f"cuda:{self.device}")
+            for proj_name in projectors:
+                state_dict = {k.split('.', 1)[1] : v for (k, v) in params.items() if k.startswith(proj_name)}
+                projectors[proj_name].load_state_dict(state_dict)
+        
+        return projectors
+    
+
+    def forward(self, inputs, outputs):
+        s_output = self.s_model(
+            inputs["s_input_ids"],
+            attention_mask=inputs.get("s_attention_mask", None),
+            position_ids=inputs.get("s_position_ids", None),
+            output_hidden_states=True
+        )
+
+        with torch.no_grad():
+            self.t_model.eval()
+            t_output = self.t_model(
+                inputs["t_input_ids"],
+                attention_mask=inputs.get("t_attention_mask", None),
+                position_ids=inputs.get("t_position_ids", None),
+                output_hidden_states=True
+            )
+
+        student_dict = {
+            "input_ids" : inputs["s_input_ids"],
+            "target_ids" : outputs["s_labels"],
+            "token_embeddings" : self.s_model.model.embed_tokens,
+            "hidden_states" : s_output.hidden_states[-1],
+            "lm_weights" : self.s_model.lm_head.weight.detach(),
+            "pad_token_id" : self.s_tokenizer.pad_token_id
+        }
+
+        teacher_dict = {
+            "input_ids" : inputs["t_input_ids"],
+            "target_ids" : outputs["t_labels"],
+            "token_embeddings" : self.t_model.model.embed_tokens,
+            "hidden_states" : t_output.hidden_states[-1],
+            "lm_weights" : self.t_model.lm_head.weight.detach(),
+            "pad_token_id" : self.t_tokenizer.pad_token_id
+        }
+
+        ce_loss = self.ce_loss_fn(s_output.logits, outputs["s_labels"])
+        kd_loss = self.kd_loss_fn(student_dict, teacher_dict, self.projectors, self.ce_loss_fn, self.kl_div_fn)
+        loss = self.weighted_loss(ce_loss, kd_loss)
+
+        return CausalLMOutput(loss=loss, logits=s_output.logits)
+
+
+
+            
+
+        
+
+
+    
+
+
+
+
+
+
--- src/dskd_loss.py
+++ src/dskd_loss.py
@@ -3,20 +3,20 @@ import torch.nn as nn
 
 
 def preprocess_model_data(model_dict):
-    model = model_dict["model"]
+    inputs = model_dict["input_ids"]
+    targets = model_dict["target_ids"]
+    mask = targets.ne(model_dict["pad_token_id"])
+
     hiddens = model_dict["hidden_states"]
+
     token_embeds = model_dict["token_embeddings"]
-
-    sequences = model_dict["sequences"]
-    inputs = sequences[..., :sequences.shape[1] - 1]
-    targets = sequences[..., 1:]
-    mask = targets.ne(model_dict["pad_id"])
-
     input_embeds = token_embeds[inputs * mask] # line 97
     target_embeds = token_embeds[targets * mask]
     embeds = torch.cat([input_embeds, target_embeds])
 
-    return model, targets, hiddens, embeds, target_embeds, mask
+    lm_weights = model_dict["lm_weights"]
+
+    return lm_weights, targets, hiddens, embeds, target_embeds, mask
 
 def norm_std(t):
     return t / t.std
@@ -27,20 +27,17 @@ def compute_attention(q, k, d, mask, masked_val=1e5):
     attention = torch.softmax(attention, -1)
     return attention
 
-def compute_projection_logits(model, projector, input, attention):
+def compute_projection_logits(lm_weights, projector, input, attention):
     proj_values = projector(input).float()
     proj_hiddens = (attention @ proj_values).to(input)
-    proj_logits = model.lm_head(proj_hiddens)
+    proj_logits = lm_weights @ proj_hiddens # TODO: does proj_hiddens @ lm_weights.transpose(-1, -2) make a difference?
     return proj_logits
 
 
-def dskd_loss(student, teacher, projectors):
-    ce_loss = nn.CrossEntropyLoss()
-    kl_div_loss = nn.KLDivLoss(reduction='none')
-
+def dskd_loss_fn(student, teacher, projectors, ce_loss, kl_div):
     # Preprocessing
-    s_model, s_targets, s_hiddens, s_embeds, s_target_embeds, s_mask = preprocess_model_data(student)
-    t_model, t_targets, t_hiddens, t_embeds, t_target_embeds, t_mask = preprocess_model_data(teacher)
+    s_lm_weights, s_targets, s_hiddens, s_embeds, s_target_embeds, s_mask = preprocess_model_data(student)
+    t_lm_weights, t_targets, t_hiddens, t_embeds, t_target_embeds, t_mask = preprocess_model_data(teacher)
 
     t_hiddens_norm = norm_std(t_hiddens)
     t_embeds_norm = norm_std(t_embeds)
@@ -52,20 +49,20 @@ def dskd_loss(student, teacher, projectors):
     keys = t_embeds_norm.float()
     
     t2s_attention = compute_attention(queries, keys, t_hiddens.shape[-1], attn_mask)
-    t2s_logits = compute_projection_logits(s_model, projectors["t2s"], t_hiddens_norm + t_target_embeds_norm, t2s_attention)
+    t2s_logits = compute_projection_logits(s_lm_weights, projectors["t2s"], t_hiddens_norm + t_target_embeds_norm, t2s_attention)
     t2s_mask = s_mask * t2s_logits.argmax(-1).eq(s_targets)
 
     s2t_attention = compute_attention(keys.transpose(-1, -2), queries.transpose(-1, -2), t_hiddens.shape[-1], attn_mask)
-    s2t_logits = compute_projection_logits(t_model, projectors["s2t"], s_hiddens, s2t_attention)
+    s2t_logits = compute_projection_logits(t_lm_weights, projectors["s2t"], s_hiddens, s2t_attention)
     s2t_mask = t_mask
 
     # Loss computation
     t2s_ce_loss = ce_loss(t2s_logits, s_targets)
 
-    t2s_kl_div = kl_div_loss(student["logits"], t2s_logits.detach()) # TODO: incorporate temperature
+    t2s_kl_div = kl_div(student["logits"], t2s_logits.detach()) # TODO: incorporate temperature
     t2s_kd_loss = (t2s_kl_div * t2s_mask).sum()
 
-    s2t_kl_div = kl_div_loss(s2t_logits, teacher["logits"])
+    s2t_kl_div = kl_div(s2t_logits, teacher["logits"])
     s2t_kd_loss = (s2t_kl_div * s2t_mask).sum()
 
     loss = t2s_ce_loss + t2s_kd_loss + s2t_kd_loss
