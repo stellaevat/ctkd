Fixed KL-divergence computation (including temperature)

src/dskd/dskd_distiller.py
@@ -8,10 +8,9 @@ from transformers.modeling_outputs import CausalLMOutput
 from dskd_loss import dskd_loss_fn
 
 
-class KD(nn.Module):
+class DSKD(nn.Module):
     def __init__(self, args, device):
-        super(KD, self).__init__()
-        self.args = args
+        super(DSKD, self).__init__()
         self.device = device
 
         self.s_model, self.s_tokenizer, self.s_hidden_size = self._load_pretrained(args.s_path, args.s_type, args.s_dtype)
@@ -19,9 +18,10 @@ class KD(nn.Module):
         self.projectors = self._load_projectors(args.proj_path)
 
         self.kd_loss_fn = dskd_loss_fn
-        self.ce_loss_fn = nn.CrossEntropyLoss()
+        self.ce_loss_fn = nn.CrossEntropyLoss(reduction='sum')
         self.kl_div_fn = nn.KLDivLoss(reduction='none')
-        self.weighted_loss = lambda ce_loss, kd_loss: (1 - args.kd_weight) * ce_loss + args.kd_weight * kd_loss
+        self.kl_temp = args.kl_temperature
+        self.weighted_loss_fn = lambda ce_loss, kd_loss: (1 - args.kd_weight) * ce_loss + args.kd_weight * kd_loss
         
 
     def _load_pretrained(self, model_path, model_type, model_dtype):
@@ -70,6 +70,7 @@ class KD(nn.Module):
                 output_hidden_states=True
             )
 
+
         student_dict = {
             "input_ids" : inputs["s_input_ids"],
             "target_ids" : outputs["s_labels"],
@@ -88,9 +89,10 @@ class KD(nn.Module):
             "pad_token_id" : self.t_tokenizer.pad_token_id
         }
 
+
         ce_loss = self.ce_loss_fn(s_output.logits, outputs["s_labels"])
-        kd_loss = self.kd_loss_fn(student_dict, teacher_dict, self.projectors, self.ce_loss_fn, self.kl_div_fn)
-        loss = self.weighted_loss(ce_loss, kd_loss)
+        kd_loss = self.kd_loss_fn(student_dict, teacher_dict, self.projectors, self.ce_loss_fn, self.kl_div_fn, self.kl_temp)
+        loss = self.weighted_loss_fn(ce_loss, kd_loss)
 
         return CausalLMOutput(loss=loss, logits=s_output.logits)
 

src/dskd/dskd_loss.py
@@ -3,18 +3,17 @@ import torch.nn as nn
 
 
 def preprocess_model_data(model_dict):
+    lm_weights = model_dict["lm_weights"]
+    hiddens = model_dict["hidden_states"]
+
     inputs = model_dict["input_ids"]
     targets = model_dict["target_ids"]
     mask = targets.ne(model_dict["pad_token_id"])
-
-    hiddens = model_dict["hidden_states"]
 
     token_embeds = model_dict["token_embeddings"]
     input_embeds = token_embeds[inputs * mask] # line 97
     target_embeds = token_embeds[targets * mask]
     embeds = torch.cat([input_embeds, target_embeds])
-
-    lm_weights = model_dict["lm_weights"]
 
     return lm_weights, targets, hiddens, embeds, target_embeds, mask
 
@@ -34,7 +33,7 @@ def compute_projection_logits(lm_weights, projector, input, attention):
     return proj_logits
 
 
-def dskd_loss_fn(student, teacher, projectors, ce_loss, kl_div):
+def dskd_loss_fn(student, teacher, projectors, ce_loss, kl_div, kl_temp):
     # Preprocessing
     s_lm_weights, s_targets, s_hiddens, s_embeds, s_target_embeds, s_mask = preprocess_model_data(student)
     t_lm_weights, t_targets, t_hiddens, t_embeds, t_target_embeds, t_mask = preprocess_model_data(teacher)
@@ -59,7 +58,7 @@ def dskd_loss_fn(student, teacher, projectors, ce_loss, kl_div):
     # Loss computation
     t2s_ce_loss = ce_loss(t2s_logits, s_targets)
 
-    t2s_kl_div = kl_div(student["logits"], t2s_logits.detach()) # TODO: incorporate temperature
+    t2s_kl_div = kl_div((student["logits"]/kl_temp).log(), t2s_logits/kl_temp)
     t2s_kd_loss = (t2s_kl_div * t2s_mask).sum()
 
     s2t_kl_div = kl_div(s2t_logits, teacher["logits"])